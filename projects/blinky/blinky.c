4d13cf5ea04fc519cdf12ff263e8433b997476cf
Peter Wittich <wittich@cornell.edu>
2019-03-04 19:04:18 -0500

    Update blinky.c
    *  reindent and update to GPIOs for the CM
    *  toggle all three LEDs
    *  Fix Makefile

--- projects/blinky/blinky.c
+++ projects/blinky/blinky.c
@@ -28,6 +28,8 @@
 #include "driverlib/debug.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
 
 //*****************************************************************************
 //
@@ -48,65 +50,140 @@
 void
 __error__(char *pcFilename, uint32_t ui32Line)
 {
-    while(1);
+  while(1);
 }
 #endif
 
 //*****************************************************************************
 //
-// Blink the on-board LED.
+// Blink the on-board LEDs
 //
 //*****************************************************************************
 int
 main(void)
 {
-    volatile uint32_t ui32Loop;
+  volatile uint32_t ui32Loop;
+
+  //
+  // Enable the GPIO ports that are used for the on-board LED.
+  // Two are J; one is P
+  //
+  MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
+
+  //
+  // Check if the peripheral access is enabled.
+  //
+  while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ)) {
+  }
+
+  // Enable the GPIO port that is used for the on-board LED.
+  //
+  MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOP);
+
+  //
+  // Check if the peripheral access is enabled.
+  //
+  while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOP)) {
+  }
+
+
+  //
+  // Enable the GPIO pin for the LEDs (PN0).
+  // Pins PJ0 (blue), PJ1 (green) and PP0 (red)
+  // Set the direction as output, and
+  // enable the GPIO pin for digital function.
+  //
+  // Configure the GPIO Pin Mux for PJ0
+  // for GPIO_PJ0
+  //
+  MAP_GPIOPinTypeGPIOOutput(GPIO_PORTJ_BASE, GPIO_PIN_0);
+    
+  //
+  // Configure the GPIO Pin Mux for PJ1
+  // for GPIO_PJ1
+  //
+  MAP_GPIOPinTypeGPIOOutput(GPIO_PORTJ_BASE, GPIO_PIN_1);
+  //
+  // Configure the GPIO Pin Mux for PP0
+  // for GPIO_PP0
+  //
+  MAP_GPIOPinTypeGPIOOutput(GPIO_PORTP_BASE, GPIO_PIN_0);
+
+
+  // original code: to be deleted
+  //GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+
+  //
+  // Loop forever.
+  //
+  while(1) {
+    //
+    // Turn on the LEDs -- RED
+    
+    // 
+    MAP_GPIOPinWrite(GPIO_PORTP_BASE, GPIO_PIN_0, GPIO_PIN_0);
 
     //
-    // Enable the GPIO port that is used for the on-board LED.
+    // Delay for a bit.
     //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
-
-    //
-    // Check if the peripheral access is enabled.
-    //
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
-    {
+    for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++) {
     }
 
     //
-    // Enable the GPIO pin for the LED (PN0).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
+    // Turn off the LED.
     //
-    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+    MAP_GPIOPinWrite(GPIO_PORTP_BASE, GPIO_PIN_0, 0x0);
+
 
     //
-    // Loop forever.
+    // Delay for a bit.
     //
-    while(1)
-    {
-        //
-        // Turn on the LED.
-        //
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
-
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
-
-        //
-        // Turn off the LED.
-        //
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0x0);
-
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
+    for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++) {
     }
+    // Turn on the LEDs -- GREEN
+    
+    // 
+    MAP_GPIOPinWrite(GPIO_PORTJ_BASE, GPIO_PIN_1, GPIO_PIN_0);
+
+    //
+    // Delay for a bit.
+    //
+    for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++) {
+    }
+
+    //
+    // Turn off the LED.
+    //
+    MAP_GPIOPinWrite(GPIO_PORTJ_BASE, GPIO_PIN_1, 0x0);
+
+
+    //
+    // Delay for a bit.
+    //
+    for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++) {
+    }
+    // Turn on the LEDs -- BLUE
+    
+    // 
+    MAP_GPIOPinWrite(GPIO_PORTJ_BASE, GPIO_PIN_0, GPIO_PIN_0);
+
+    //
+    // Delay for a bit.
+    //
+    for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++) {
+    }
+
+    //
+    // Turn off the LED.
+    //
+    MAP_GPIOPinWrite(GPIO_PORTJ_BASE, GPIO_PIN_0, 0x0);
+
+
+    //
+    // Delay for a bit.
+    //
+    for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++) {
+    }
+    
+  }
 }
