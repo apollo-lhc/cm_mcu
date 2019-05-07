d36fb738ca819b5841e07bdc41e28c13d34b9a0f
Peter Wittich <wittich@cornell.edu>
2019-05-07 10:18:46 -0400

    Update i2c_reg.h
    include standard header file for relevant types

--- projects/project2/i2c_reg.h
+++ projects/project2/i2c_reg.h
@@ -8,6 +8,9 @@
 #ifndef PROJECTS_PROJECT2_I2C_REG_H_
 #define PROJECTS_PROJECT2_I2C_REG_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+
 void initI2C1(void);
 
 bool writeI2C(uint8_t ui8Addr, uint8_t ui8Reg, uint8_t *Data, const uint8_t ui8ByteCount);
