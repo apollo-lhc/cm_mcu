edab2a7e49b2f124ea0dd1448119b1784557d97a
Peter Wittich <wittich@cornell.edu>
2019-05-09 17:16:29 -0400

    Update project2.c
    mainly whitespace fixes, ugh

--- projects/project2/project2.c
+++ projects/project2/project2.c
@@ -97,12 +97,12 @@ void UARTIntHandler( void )
   int received = 0;
   while(ROM_UARTCharsAvail(UART4_BASE)) {
 
-      bytes[received] = (uint8_t)ROM_UARTCharGetNonBlocking(UART4_BASE);
-      // Put byte in queue (ISR safe function) -- should probably send more than one byte at a time?
-      if ( ++received == 8 ) {
-	  xStreamBufferSendFromISR(xStreamBuffer, &bytes, 8, &xHigherPriorityTaskWoken);
-	  received = 0;
-      }
+    bytes[received] = (uint8_t)ROM_UARTCharGetNonBlocking(UART4_BASE);
+    // Put byte in queue (ISR safe function) -- should probably send more than one byte at a time?
+    if ( ++received == 8 ) {
+      xStreamBufferSendFromISR(xStreamBuffer, &bytes, 8, &xHigherPriorityTaskWoken);
+      received = 0;
+    }
   }
   if ( received )
     xStreamBufferSendFromISR(xStreamBuffer, &bytes, 8, &xHigherPriorityTaskWoken);
@@ -143,29 +143,29 @@ UARTIntHandler2(void)
   // Loop while there are characters in the receive FIFO.
   //
   while(ROM_UARTCharsAvail(UART4_BASE))
-    {
-      //
-      // Read the next character from the UART and write it back to the UART.
-      //
-      ROM_UARTCharPutNonBlocking(UART4_BASE,
-				 ROM_UARTCharGetNonBlocking(UART4_BASE));
+  {
+    //
+    // Read the next character from the UART and write it back to the UART.
+    //
+    ROM_UARTCharPutNonBlocking(UART4_BASE,
+        ROM_UARTCharGetNonBlocking(UART4_BASE));
 
-      //
-      // Blink the LED to show a character transfer is occurring.
-      //
-      MAP_GPIOPinWrite(USER_LED12_PORT, USER_LED2_PIN, USER_LED2_PIN);
+    //
+    // Blink the LED to show a character transfer is occurring.
+    //
+    MAP_GPIOPinWrite(USER_LED12_PORT, USER_LED2_PIN, USER_LED2_PIN);
 
-      //
-      // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
-      //
-      SysCtlDelay(g_ui32SysClock / (1000 * 3));
+    //
+    // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
+    //
+    SysCtlDelay(g_ui32SysClock / (1000 * 3));
 
-      //
-      // Turn off the LED
-      //
-      MAP_GPIOPinWrite(USER_LED12_PORT, USER_LED2_PIN, 0x0);
+    //
+    // Turn off the LED
+    //
+    MAP_GPIOPinWrite(USER_LED12_PORT, USER_LED2_PIN, 0x0);
 
-    }
+  }
 }
 
 
@@ -179,13 +179,13 @@ void SystemInit()
   // Run from the PLL at 120 MHz.
   //
   g_ui32SysClock = SysCtlClockFreqSet((SYSCTL_OSC_INT |
-                                       SYSCTL_USE_PLL |
-                                       SYSCTL_CFG_VCO_480), 120000000);
+      SYSCTL_USE_PLL |
+      SYSCTL_CFG_VCO_480), configCPU_CLOCK_HZ);
   UART4Init(g_ui32SysClock);
   initI2C1(g_ui32SysClock);
 
   // SYSTICK timer
-  MAP_SysTickPeriodSet(100); // period in Hz
+  MAP_SysTickPeriodSet(configTICK_RATE_HZ); // period in Hz
   MAP_SysTickIntEnable(); // enable the interrupt
   MAP_SysTickEnable();
   return;
@@ -199,30 +199,30 @@ volatile uint32_t g_ui32SysTickCount;
 // Holds the handle of the created queue.
 static QueueHandle_t xLedQueue = NULL;
 
-#define PS_BAD 0x03;
+#define PS_BAD  0x02;
 #define PS_GOOD 0x01;
 
 // control the LED
 void LedTask(void *parameters)
 {
   // TODO: this should also handle blinking, by using a non-blocking queue check and a vTaskDelayUntil call
-	TickType_t xLastWakeTime = xTaskGetTickCount();
+  TickType_t xLastWakeTime = xTaskGetTickCount();
   uint32_t result;
   // this function never returns
   for ( ;; ) {
-	  // check for a new item in the queue but don't wait
-	  xQueueReceive(xLedQueue, &result, 0);
-	  switch (result ) {
-	  case 0x00:
-		  break;
-	  case 0x01:
-		  // do something
-		  break;
-	  default:
-		  break;
-	  }
-	  // wait for next check
-	  vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( 250 ) );
+    // check for a new item in the queue but don't wait
+    xQueueReceive(xLedQueue, &result, 0);
+    switch (result ) {
+    case 0x00:
+      break;
+    case 0x01:
+      // do something
+      break;
+    default:
+      break;
+    }
+    // wait for next check
+    vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( 250 ) );
 
   }
 }
@@ -235,19 +235,19 @@ void PowerSupplyTask(void *parameters)
 
   // this function never returns
   for ( ;; ) {
-	  uint32_t message;
-	  bool good = check_ps();
-	  if ( ! good ) {
-		  message = PS_BAD;
-		  lastStateGood = false;
-	  }
-	  else if ( !lastStateGood ) { // good now, was bad
-		  message = PS_GOOD;
-		  lastStateGood = true;
-	  }
-	  xQueueSendToBack(xLedQueue, &message, pdMS_TO_TICKS(10));
+    uint32_t message;
+    bool good = check_ps();
+    if ( ! good ) {
+      message = PS_BAD;
+      lastStateGood = false;
+    }
+    else if ( !lastStateGood ) { // good now, was bad
+      message = PS_GOOD;
+      lastStateGood = true;
+    }
+    xQueueSendToBack(xLedQueue, &message, pdMS_TO_TICKS(10));
 
-	  vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( 250 ) );
+    vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( 250 ) );
   }
 }
 
@@ -265,7 +265,7 @@ int main( void )
 
   // queue for the LED
   xLedQueue = xQueueCreate(5, // The maximum number of items the queue can hold.
-			   sizeof( uint32_t )); 		// The size of each item.
+      sizeof( uint32_t )); 		// The size of each item.
 
 
   // start the tasks here 
@@ -295,7 +295,7 @@ void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
 
 void vApplicationIdleHook( void )
 {
-   // not doing anything right now; this is just here in case I need to use it
+  // not doing anything right now; this is just here in case I need to use it
 }
 
 
