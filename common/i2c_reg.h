649190ab54109a1089edc2b05bfcff034e17c83c
Peter Wittich <wittich@cornell.edu>
2019-05-14 12:17:40 -0400

    Update i2c_reg.h
    fix incorrect prototype

--- common/i2c_reg.h
+++ common/i2c_reg.h
@@ -13,8 +13,8 @@
 
 void initI2C1(const uint32_t sysclockfreq);
 
-bool writeI2C(uint32_t i2cbase, uint8_t ui8Addr, uint8_t ui8Reg, uint8_t *Data, const uint8_t ui8ByteCount);
-
+bool writeI2Creg(uint32_t i2cbase, uint8_t ui8Addr, uint8_t ui8Reg, uint8_t *Data,
+    const uint8_t ui8ByteCount);
 bool readI2Creg(uint32_t i2cbase, uint8_t ui8Addr, uint8_t ui8Reg, uint8_t *Data, const uint8_t ui8ByteCount);
 
 
