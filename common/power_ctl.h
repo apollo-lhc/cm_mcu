0f2c13e79537c720c69a37790d3cb3a426360b50
Peter Wittich <wittich@cornell.edu>
2019-05-14 09:06:49 -0400

    more messages for the LED message queue

--- common/power_ctl.h
+++ common/power_ctl.h
@@ -11,11 +11,17 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-
+// these are all LED messages
 #define PS_GOOD    (1)
 #define PS_BAD     (2)
 #define PS_ON      (3)
 #define PS_OFF     (4)
+// this should go elsewhere
+#define RED_LED_OFF     (5)
+#define RED_LED_ON      (6)
+#define RED_LED_TOGGLE  (7)
+#define RED_LED_TOGGLE3 (8)
+#define RED_LED_TOGGLE4 (9)
 
 
 bool set_ps(bool KU15P, bool VU7PMGT1, bool VU7PMGT2);
