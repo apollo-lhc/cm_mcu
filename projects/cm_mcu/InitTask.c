98f56437bc01a37ba13fde8eccca5ca882879cdc
gwengardner <glg62@cornell.edu>
2020-02-14 12:58:54 -0500

    InitTask.c

---
+++ projects/cm_mcu/InitTask.c
@@ -0,0 +1,31 @@
+/*
+ * InitTask.c
+ *
+ *  Created on: Feb 7, 2020
+ *      Author: glg62
+ */
+
+// includes for types
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+
+#include "driverlib/eeprom.h"
+#include "common/utils.h"
+#include "Tasks.h"
+
+
+void InitTask(void *parameters){
+
+	// Initialize eeprom error buffer
+	errbuffer_init(ebuf,EBUF_MINBLK,EBUF_MAXBLK);
+	errbuffer_put(ebuf,RESTART,0);
+
+
+	// Delete this task
+	vTaskDelete(xTaskGetCurrentTaskHandle());
+}
