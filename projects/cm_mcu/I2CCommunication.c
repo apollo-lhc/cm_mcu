785ce9182630c9c067d5338bf0e0d5f5f126618b
Peter Wittich <wittich@cornell.edu>
2022-05-04 16:45:34 -0400

    bug in stuck i2c check fix

--- projects/cm_mcu/I2CCommunication.c
+++ projects/cm_mcu/I2CCommunication.c
@@ -96,7 +96,7 @@ int apollo_i2c_ctl_r(uint8_t device, uint8_t address, uint8_t nbytes, uint8_t da
     int tries = 0;
     while (SMBusStatusGet(p_sMaster) == SMBUS_TRANSFER_IN_PROGRESS) {
       vTaskDelay(pdMS_TO_TICKS(10));
-      if ( tries > 100 ) {
+      if ( tries++ > 100 ) {
         log_warn(LOG_I2C, "transfer stuck\r\n");
         break;
       }
@@ -133,7 +133,7 @@ int apollo_i2c_ctl_reg_r(uint8_t device, uint8_t address, uint8_t nbytes_addr,
     int tries = 0;
     while (SMBusStatusGet(smbus) == SMBUS_TRANSFER_IN_PROGRESS) {
       vTaskDelay(pdMS_TO_TICKS(10));
-      if ( tries > 100 ) {
+      if ( tries++ > 100 ) {
         log_warn(LOG_I2C, "transfer stuck\r\n");
         break;
       }
@@ -184,7 +184,7 @@ int apollo_i2c_ctl_reg_w(uint8_t device, uint8_t address, uint8_t nbytes_addr, u
     int tries = 0;
     while (SMBusStatusGet(p_sMaster) == SMBUS_TRANSFER_IN_PROGRESS) {
       vTaskDelay(pdMS_TO_TICKS(10));
-      if ( tries > 100 ) {
+      if ( tries++ > 100 ) {
         log_warn(LOG_I2C, "transfer stuck\r\n");
         break;
       }
@@ -222,7 +222,7 @@ int apollo_i2c_ctl_w(uint8_t device, uint8_t address, uint8_t nbytes, int value)
     int tries = 0;
     while (SMBusStatusGet(p_sMaster) == SMBUS_TRANSFER_IN_PROGRESS) {
       vTaskDelay(pdMS_TO_TICKS(10));
-      if ( tries > 100 ) {
+      if ( tries++ > 100 ) {
         log_warn(LOG_I2C, "transfer stuck\r\n");
         break;
       }
@@ -248,7 +248,7 @@ int apollo_pmbus_rw(tSMBus *smbus, volatile tSMBusStatus *smbus_status, bool rea
   int tries = 0;
   while (SMBusStatusGet(smbus) == SMBUS_TRANSFER_IN_PROGRESS) {
     vTaskDelay(pdMS_TO_TICKS(10));
-    if ( tries > 100 ) {
+    if ( tries++ > 100 ) {
       log_warn(LOG_I2C, "transfer stuck\r\n");
       break;
     }
@@ -269,7 +269,7 @@ int apollo_pmbus_rw(tSMBus *smbus, volatile tSMBusStatus *smbus_status, bool rea
   tries = 0;
   while (SMBusStatusGet(smbus) == SMBUS_TRANSFER_IN_PROGRESS) {
     vTaskDelay(pdMS_TO_TICKS(10));
-    if ( tries > 100 ) {
+    if ( tries++ > 100 ) {
       log_warn(LOG_I2C, "transfer stuck\r\n");
       break;
     }
