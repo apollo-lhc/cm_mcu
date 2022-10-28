cea0f1b8833ed512f2b28170c5869bc5ba439eaa
Peace Kotamnives <pk568@cornell.edu>
2022-10-28 15:27:23 -0400

    fix clang format

--- projects/cm_mcu/CommandLineTask.c
+++ projects/cm_mcu/CommandLineTask.c
@@ -111,8 +111,8 @@ static BaseType_t init_load_clock_ctl(int argc, char **argv, char *m)
   bool isFullyPowered = false; // assume not fully powered
   enum power_system_state power_state = getPowerControlState();
   if (power_state != POWER_ON) { // if the power state is not fully on
-    if (!isFullyPowered) {        // was previously on
-      copied += snprintf(m + copied, SCRATCH_SIZE - copied," 3V3 died. skip loadclock\r\n");
+    if (!isFullyPowered) {       // was previously on
+      copied += snprintf(m + copied, SCRATCH_SIZE - copied, " 3V3 died. skip loadclock\r\n");
       isFullyPowered = false;
     }
     snprintf(m + copied, SCRATCH_SIZE - copied, "%s operation failed \r\n", argv[0]);
