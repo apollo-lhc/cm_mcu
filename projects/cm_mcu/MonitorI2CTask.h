01c42c26608e5e5fbe4e5c2ceefd851f1441fd40
Peace Kotamnives <pk568@cornell.edu>
2023-08-09 07:24:26 -0400

    fix clang format

--- projects/cm_mcu/MonitorI2CTask.h
+++ projects/cm_mcu/MonitorI2CTask.h
@@ -42,7 +42,7 @@ struct MonitorI2CTaskArgs_t {
   TickType_t updateTick;               // last update time, in ticks
   SemaphoreHandle_t xSem;              // semaphore for controlling access to device
   uint8_t ffpart_bit_mask;             // this mask is only used for detecting 12-ch 25Gbps on the REV2 board
-  uint32_t present_bit_mask;            // this mask is used for all ffs to detect if it is mounted or not
+  uint32_t present_bit_mask;           // this mask is used for all ffs to detect if it is mounted or not
   const uint8_t n_rxchs;               // the number of optical receiver channels
   uint16_t *opt_pow_values;            // a set of optical power measurements only from FFs with 25Gbs
   UBaseType_t stack_size;              // stack size of task
