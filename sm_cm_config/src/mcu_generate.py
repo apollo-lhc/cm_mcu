db5d4a1cb6bc1c9e9365c4919a7eb1a2d8fed261
Peace Kotamnives <pk568@cornell.edu>
2023-09-08 14:29:10 -0400

    apply clang format (mostly to sm_cm_config)

--- sm_cm_config/src/mcu_generate.py
+++ sm_cm_config/src/mcu_generate.py
@@ -1,34 +1,17 @@
-import yaml
-from pprint import pprint
+import yaml from pprint import pprint
 #import dicttoxml
-# there is also dict2xml available in pypy (python3)
+#there is also dict2xml available in pypy(python3)
 
+    with open('../data/zynqmon_2.yml') as f:data = yaml.load(f, Loader = yaml.FullLoader)
 
+                                                                    config = data['config'] for c in config:print(c['name'])
 
-with open('../data/zynqmon_2.yml') as f:
-    data = yaml.load(f, Loader=yaml.FullLoader)
-
-
-config = data['config']
-for c in config:
-    print(c['name'])
-
-
-size = 0
-for c in config:
-    # generate C call
-    expected_length = len(c['names'])
-    if 'postfixes' in c:
-        expected_length *= len(c['postfixes'])
-    if '32' in c['type']:
-        expected_length *= 2
-    elif 'char' in c['type']:
-        expected_length = expected_length/2.
-    if 'size' in c:
-        expected_length *= c['size']*4 # size in 4 byte words -- extra factor of 4
-    #if ( c['count'] != expected_length ) :
-    #    print(f"// mismatch:  {expected_length}, size {c['count']}")
-    print(f"// {c['name']}, size {expected_length}")
-    print(f"zm_set_{c['mcu_call']}(&zynqmon_data[{size}], {c['start']});")
-    size += int(expected_length)
-print(f"#define ZMON_VALID_ENTRIES {size}")
+                                                                        size = 0 for c in config:
+#generate C call
+                                                             expected_length = len(c['names']) if 'postfixes' in c:expected_length *= len(c['postfixes']) if '32' in c['type'] :expected_length *= 2 elif 'char' in c['type'] :expected_length = expected_length / 2. if 'size' in c:expected_length *= c['size'] * 4 #size in 4 byte words -- extra factor of 4
+#if (c['count'] != expected_length) :
+#print(f "// mismatch:  {expected_length}, size {c['count']}")
+                                                             print(f "// {c['name']}, size {expected_length}")
+                                                                 print(f "zm_set_{c['mcu_call']}(&zynqmon_data[{size}], {c['start']});")
+                                                                     size += int(expected_length)
+                                                                         print(f "#define ZMON_VALID_ENTRIES {size}")
