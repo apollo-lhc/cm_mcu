1134019da539e3318c4a9359a873d2d130f0f587
Peter Wittich <wittich@cornell.edu>
2024-04-18 12:51:36 -0400

    Update ZynqMonTask.c

--- projects/cm_mcu/ZynqMonTask.c
+++ projects/cm_mcu/ZynqMonTask.c
@@ -305,6 +305,7 @@ void zm_set_firefly_temps(struct zynqmon_data_t data[], int start)
 uint16_t getFFtXdisenablebit(const uint8_t i)
 {
   return 56; // FIXME
+#ifdef NOTDEF
   if (i > NFIREFLIES_F1 + NFIREFLIES_F2) {
     log_warn(LOG_SERVICE, "caught %d > total fireflies %d\r\n", i, NFIREFLIES);
     return 56;
@@ -330,6 +331,7 @@ uint16_t getFFtXdisenablebit(const uint8_t i)
     return 56;
   else
     return val;
+#endif // NOTDEF
 }
 // updated once per loop.
 // For each firefly device, send
