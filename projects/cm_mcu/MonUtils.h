59cf55250cfceb65d0ca1429c33db4fdedf0e70d
Peter Wittich <wittich@cornell.edu>
2024-05-20 17:04:12 -0400

    missing header

---
+++ projects/cm_mcu/MonUtils.h
@@ -0,0 +1,10 @@
+#ifndef MONUTILS_H
+#define MONUTILS_H
+
+#include "MonitorTaskI2C_new.h"
+
+extern struct MonitorI2CTaskArgs_new_t ff_f1_args;
+extern struct MonitorI2CTaskArgs_new_t ff_f2_args;
+
+#endif // MONUTILS_H
+
