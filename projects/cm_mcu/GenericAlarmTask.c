d0080d4114c6651d5eed02c3c6a6503dfc0b05f6
Peter Wittich <wittich@cornell.edu>
2024-07-15 09:16:15 -0500

    add a state to alarm state machine

--- projects/cm_mcu/GenericAlarmTask.c
+++ projects/cm_mcu/GenericAlarmTask.c
@@ -12,12 +12,14 @@
 #include "common/power_ctl.h"
 
 #include "AlarmUtilities.h"
+#include <stdbool.h>
 
 #define X_MACRO_ALM_STATES \
-    X(ALM_INIT) \
+    X(ALM_INIT)   \
     X(ALM_NORMAL) \
-    X(ALM_WARN) \
-    X(ALM_ERROR)
+    X(ALM_WARN)   \
+    X(ALM_FAULT_ERRORING)  \
+    X(ALM_FAULT_ERROR_CLEARED)
 
 enum alarm_task_state {
 #define X(name) name,
@@ -38,15 +40,17 @@ static const char *alarm_task_state_names[] = {
 // +---+--+
 //     |
 //     |
-//  +--+--------+      +---------+      +-------+
-//  |   NORMAL  +----->+  WARN   +----->+ ERROR |
-//  +-----+-----+      +-+-------+      +---+---+
-//        ^              |                  |
-//        +--------------+                  |
-//        ^                                 |
-//        +---------------------------------+
+//  +--+--------+      +---------+      +-------+    +-------+
+//  |   NORMAL  +----->+  WARN   +----->+ FAULT |--->| ERR_c |
+//  +-----+-----+      +-+-------+      +-------+    +---+---+
+//        ^              |                     	       	 |
+//        +--------------+                   		 |
+//        ^                                  		 |
+//        +------------------------------------- --------+
 // once we get into the ERROR state the only way to clear an error is via a message
 // sent to the CLI.
+// Fault_erroring means the error is active and we are in a fault state
+// fault_error_cleared means the error has cleared but the fault remains. 
 //
 
 
@@ -59,17 +63,17 @@ void GenericAlarmTask(void *parameters)
   // initialize to the current tick time
   TickType_t xLastWakeTime = xTaskGetTickCount();
   uint32_t message; // this must be in a semi-permanent scope
-  bool alarming;
+  bool external_reset = false;
 
   enum alarm_task_state currentState = ALM_INIT;
   for (;;) {
     vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(50));
     if (xQueueReceive(params->xAlmQueue, &message, 0)) {
-      log_debug(LOG_ALM, "%s: received message %d (%s)\r\n", taskName, message, 
+      log_debug(LOG_ALM, "%s: received message %d (%s)\r\n", taskName, message,
           msgqueue_message_text[message]);
       switch (message) {
         case ALM_CLEAR_ALL: // clear all alarms
-          alarming = false;
+          external_reset = true;
           break;
         default:
           break;
@@ -90,11 +94,9 @@ void GenericAlarmTask(void *parameters)
       case ALM_NORMAL: {
         if (status) {
           params->errorlog_registererror();
-          alarming = true;
           nextState = ALM_WARN;
         }
         else {
-          alarming = false;
           nextState = ALM_NORMAL;
         }
         break;
@@ -104,7 +106,6 @@ void GenericAlarmTask(void *parameters)
           // we are back to normal
           params->errorlog_clearerror();
           nextState = ALM_NORMAL;
-          alarming = false;
         }
         else if (status > 1) {
           // log alarm, send message to turn off power and move to error state
@@ -114,35 +115,45 @@ void GenericAlarmTask(void *parameters)
           // this message always goes to the power queue, for all
           // alarms.
           xQueueSendToFront(xPwrQueue, &message, 100);
-          log_debug(LOG_ALM, "sent message %d (%s) to power queue\r\n", TEMP_ALARM, 
+          log_debug(LOG_ALM, "sent message %d (%s) to power queue\r\n", TEMP_ALARM,
               msgqueue_message_text[TEMP_ALARM]);
-          nextState = ALM_ERROR;
+          nextState = ALM_FAULT_ERRORING;
         }
         else {
           nextState = ALM_WARN;
         }
         break;
       }
-      case ALM_ERROR: {
-        if (!alarming && !status) {
-          // error has cleared, log and move to normal state
+      case ALM_FAULT_ERRORING: {
+        if (!status) {
+          // error has cleared, log and move to fault state
           if (params->errorlog_clearerror)
             params->errorlog_clearerror();
           message = TEMP_ALARM_CLEAR;
           // this message always goes to the power queue, for all
           // alarms.
           xQueueSendToFront(xPwrQueue, &message, 100);
-          log_debug(LOG_ALM, "sent message %d (%s) to power queue\r\n", TEMP_ALARM_CLEAR,
-                    msgqueue_message_text[TEMP_ALARM_CLEAR]);
+          log_debug(LOG_ALM, "sent message %d (%s) to power queue\r\n",
+		                TEMP_ALARM_CLEAR, msgqueue_message_text[TEMP_ALARM_CLEAR]);
+          nextState = ALM_FAULT_ERROR_CLEARED;
+        }
+        else {
+          nextState = ALM_FAULT_ERRORING;
+        }
+        break;
+      }
+      case ALM_FAULT_ERROR_CLEARED: {
+        if (external_reset) {
+          external_reset = false;
           nextState = ALM_NORMAL;
         }
         else {
-          nextState = ALM_ERROR;
+          nextState = ALM_FAULT_ERROR_CLEARED;
         }
         break;
       }
       default:
-        nextState = ALM_ERROR;
+        nextState = ALM_FAULT_ERRORING;
         break;
     }
     if (currentState != nextState) {
