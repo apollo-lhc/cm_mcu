ddd31fbca23776cfe96737c06966d28039ad2b53
Michael Oshiro <oshiro@ucsb.edu>
2025-01-03 12:30:47 -0500

    Made first pass at DC-DC I2C test

--- projects/prod_test/PowerI2CCommands.c
+++ projects/prod_test/PowerI2CCommands.c
@@ -52,7 +52,6 @@ BaseType_t run_dcdc_i2ctest(int argc, char **argv, char *m)
   // initialize to the current tick time
   TickType_t xLastWakeTime = xTaskGetTickCount();
   uint8_t data[2];
-  float vals[N_PM_ADDRS_DCDC*NPAGES_PS];
 
   //// attempt to grab semaphore
   //// TODO define semaphores including i2c1 somewhere
@@ -61,106 +60,127 @@ BaseType_t run_dcdc_i2ctest(int argc, char **argv, char *m)
   //  return pdFALSE;
   //}
 
-  // loop over devices
-  for (int ps = 0; ps < N_PM_ADDRS_DCDC; ++ps) {
+  // do two passes, write the first time and read the second
+  for (uint8_t rw = 0; rw < 2; ++rw) {
 
-    // select the appropriate output for the mux
-    data[0] = 0x1U << pm_addrs_dcdc[ps].mux_bit;
-    tSMBusStatus r = SMBusMasterI2CWrite(&g_sMaster1, 
-                                         pm_addrs_dcdc[ps].mux_addr, data, 1);
-    if (r != SMBUS_OK) {
-      snprintf(m, SCRATCH_SIZE, "Could not select dev=%d on MUX\r\n", ps);
-      //release_semaphore();
-      return pdFALSE;
-    }
-    int tries = 0;
-    while (SMBusStatusGet(&g_sMaster1) == SMBUS_TRANSFER_IN_PROGRESS) {
-      vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10)); // wait
-      if (++tries > 500) {
-        snprintf(m, SCRATCH_SIZE, "MUX select timed out (dev=%d)\r\n", ps);
-        //release_semaphore();
-        return pdFALSE;
-      }
-    }
-    if (eStatus1 != SMBUS_OK) {
-      snprintf(m, SCRATCH_SIZE, "SM bad status on MUX select (dev=%d)\r\n", 
-               ps);
-      //release_semaphore();
-      return pdFALSE;
-    }
+    // loop over devices
+    for (uint8_t ps = 0; ps < N_PM_ADDRS_DCDC; ++ps) {
 
-    // loop over pages on the supply
-    for (uint8_t page = 0; page < NPAGES_PS; ++page) {
-      // select page
-      r = SMBusMasterByteWordWrite(&g_sMaster1, pm_addrs_dcdc[ps].dev_addr, 
-                                   PAGE_COMMAND, &page, 1);
-      if (r != SMBUS_OK) {
-        snprintf(m, SCRATCH_SIZE, "Could not select page=%d on dev=%d\r\n", 
-                 page, ps);
-        //release_semaphore();
-        return pdFALSE;
-      }
-      while (SMBusStatusGet(&g_sMaster1) == SMBUS_TRANSFER_IN_PROGRESS) {
-        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10)); // wait
-      }
-      if (eStatus1 != SMBUS_OK) {
-        snprintf(m, SCRATCH_SIZE, 
-                 "SM bad status on page=%d select (dev=%d)\r\n", page, ps);
-        //release_semaphore();
-        return pdFALSE;
-      }
-
-      // For now, just do run read to test
-      // TODO find a register than we can use for read/write test
-      // Use 8D (Internal temperature)
-      data[0] = 0x0U;
-      data[1] = 0x0U;
-      r = SMBusMasterByteWordRead(&g_sMaster1, pm_addrs_dcdc[ps].dev_addr,
-                                  0x8d, data, 2);
+      // select the appropriate output for the mux
+      data[0] = 0x1U << pm_addrs_dcdc[ps].mux_bit;
+      tSMBusStatus r = SMBusMasterI2CWrite(&g_sMaster1, 
+                                           pm_addrs_dcdc[ps].mux_addr, data, 
+                                           1);
       if (r != SMBUS_OK) {
         snprintf(m, SCRATCH_SIZE, 
-                 "Read failed (page=%d dev=%d)\r\n", page, ps);
+                 "ERROR: Could not select dev=%d on MUX\r\n", ps);
         //release_semaphore();
         return pdFALSE;
       }
-      tries = 0;
+      int tries = 0;
       while (SMBusStatusGet(&g_sMaster1) == SMBUS_TRANSFER_IN_PROGRESS) {
         vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10)); // wait
         if (++tries > 500) {
           snprintf(m, SCRATCH_SIZE, 
-                   "Read timed out (page=%d dev=%d)\r\n", page, ps);
+                   "ERROR: MUX select timed out (dev=%d)\r\n", ps);
           //release_semaphore();
           return pdFALSE;
         }
       }
       if (eStatus1 != SMBUS_OK) {
         snprintf(m, SCRATCH_SIZE, 
-                 "SM bad status after read (page=%d dev=%d)\r\n", page, ps);
+                 "ERROR: SM bad status on MUX select (dev=%d)\r\n", 
+                 ps);
         //release_semaphore();
         return pdFALSE;
       }
-      linear11_val_t ii;
-      ii.raw = (data[1] << 8) | data[0];
-      vals[page+ps*NPAGES_PS] = linear11_to_float(ii);
 
-      // wait here for the 10 msec
-      vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
-    } // loop over pages on the supply
-  } // loop over devices
+      // loop over pages on the supply
+      // do we actually need this? If our goal is just to test the hardware I2C
+      // connections, we could just use the first controller on each LGA80D
+      for (uint8_t page = 0; page < NPAGES_PS; ++page) {
+        // select page
+        r = SMBusMasterByteWordWrite(&g_sMaster1, pm_addrs_dcdc[ps].dev_addr, 
+                                     PAGE_COMMAND, &page, 1);
+        if (r != SMBUS_OK) {
+          snprintf(m, SCRATCH_SIZE, 
+                   "ERROR: Could not select page=%d on dev=%d\r\n", page, ps);
+          //release_semaphore();
+          return pdFALSE;
+        }
+        while (SMBusStatusGet(&g_sMaster1) == SMBUS_TRANSFER_IN_PROGRESS) {
+          vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10)); // wait
+        }
+        if (eStatus1 != SMBUS_OK) {
+          snprintf(m, SCRATCH_SIZE, 
+                   "ERROR: SM bad status on page=%d select (dev=%d)\r\n", page,
+                   ps);
+          //release_semaphore();
+          return pdFALSE;
+        }
+
+        // Use B0 (USER_DATA_00)
+        if (rw == 0) { // write on first pass
+          data[0] = page+ps*NPAGES_PS+1;
+          data[1] = 0x3CU;
+          r = SMBusMasterByteWordWrite(&g_sMaster1, pm_addrs_dcdc[ps].dev_addr, 
+                                       0xb0, data, 2);
+        }
+        else { // read on second pass
+          data[0] = 0x0U;
+          data[1] = 0x0U;
+          r = SMBusMasterByteWordRead(&g_sMaster1, pm_addrs_dcdc[ps].dev_addr,
+                                      0xb0, data, 2);
+        }
+        if (r != SMBUS_OK) {
+          snprintf(m, SCRATCH_SIZE, 
+                   "ERROR: Read failed (page=%d dev=%d)\r\n", page, ps);
+          //release_semaphore();
+          return pdFALSE;
+        }
+        tries = 0;
+        while (SMBusStatusGet(&g_sMaster1) == SMBUS_TRANSFER_IN_PROGRESS) {
+          vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10)); // wait
+          if (++tries > 500) {
+            snprintf(m, SCRATCH_SIZE, 
+                     "ERROR: Read timed out (page=%d dev=%d)\r\n", page, ps);
+            //release_semaphore();
+            return pdFALSE;
+          }
+        }
+        if (eStatus1 != SMBUS_OK) {
+          snprintf(m, SCRATCH_SIZE, 
+                   "ERROR: SM bad status after read (page=%d dev=%d)\r\n", 
+                   page, ps);
+          //release_semaphore();
+          return pdFALSE;
+        }
+        if (rw == 0) { 
+          // check read value
+          if (data[0] != page+ps*NPAGES_PS+1) {
+            snprintf(m, SCRATCH_SIZE, 
+                     "ERROR: Bad bit 0 on dev %d-%d (expected %d, got %d)\r\n", 
+                     ps, page, page+ps*NPAGES_PS+1, data[0]);
+            //release_semaphore();
+            return pdFALSE;
+          }
+          if (data[1] != 0x3CU) {
+            snprintf(m, SCRATCH_SIZE, 
+                     "ERROR: Bad bit 1 on dev %d-%d (expected 60, got %d)\r\n", 
+                     ps, page, data[1]);
+            //release_semaphore();
+            return pdFALSE;
+          }
+        }
+
+        // wait here for the 10 msec
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
+      } // loop over pages on the supply
+    } // loop over devices
+  } // read/write passes
 
   //print output
-  int tens[N_PM_ADDRS_DCDC*NPAGES_PS];
-  int frac[N_PM_ADDRS_DCDC*NPAGES_PS];
-  for (uint8_t idevpg = 0; idevpg < N_PM_ADDRS_DCDC*NPAGES_PS; ++idevpg) {
-    float_to_ints(vals[idevpg], &tens[idevpg], &frac[idevpg]);
-  }
-  snprintf(m, SCRATCH_SIZE, 
-           "Test success. read temperatures(C): (%02d.%02d, %02d.%02d, %02d.%02d, %02d.%02d, %02d.%02d %02d.%02d, %02d.%02d, %02d.%02d, %02d.%02d, %02d.%02d, %02d.%02d, %02d.%02d, %02d.%02d, %02d.%02d)\r\n", 
-           tens[0], frac[0], tens[1], frac[1], tens[2], frac[2],
-           tens[3], frac[3], tens[4], frac[4], tens[5], frac[5],
-           tens[6], frac[6], tens[7], frac[7], tens[8], frac[8],
-           tens[9], frac[9], tens[10], frac[10], tens[11], frac[11],
-           tens[12], frac[12], tens[13], frac[13]);
+  snprintf(m, SCRATCH_SIZE, "Test success.\r\n");
   //release_semaphore();
   return pdFALSE;
 }   
