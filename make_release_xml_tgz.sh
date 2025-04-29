001c4ef9767b855e80dd3e83ff3aac76c9a37fb0
Peter Wittich <wittich@cornell.edu>
2025-04-29 13:02:37 -0400

    simple update to release generation

--- make_release_xml_tgz.sh
+++ make_release_xml_tgz.sh
@@ -8,6 +8,13 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
 
+# choose which release this build is for
+REV="2" # default
+if [ "${REV1}" == "1" ]; then
+    REV="1"
+elif [ "${REV3}" == "1" ]; then
+    REV="3"
+fi
 
 [ -d tmp ] && rm -rf tmp
 
@@ -16,7 +23,7 @@ cd tmp
 cp ../PL_MEM*.xml CornellCM_MCU/address_table/modules_CM_MCU
 cp ../sm_cm_config/data/*.xml CornellCM_MCU/address_table
 cd CornellCM_MCU/address_table/modules_CM_MCU
-ln -s PL_MEM_CM_Rev2.xml PL_MEM_CM.xml
+ln -s PL_MEM_CM_Rev${REV}.xml PL_MEM_CM.xml
 cd ../../..
 tar -czf ../CornellCM_MCU.tar.gz CornellCM_MCU
 cd ..
