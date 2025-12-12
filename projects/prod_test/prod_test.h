1882d8d86ff3f3b72372e7769bc33915eff08f17
Peter Wittich <wittich@cornell.edu>
2025-12-12 10:00:22 -0500

    add missing build and version command prototypes

--- projects/prod_test/prod_test.h
+++ projects/prod_test/prod_test.h
@@ -3,10 +3,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include <sys/_intsup.h>
 #define SYSTEM_STACK_SIZE  128
 #define configCPU_CLOCK_HZ 40000000
 
 extern uint32_t g_ui32SysClock;
+
+const char* buildTime(void);
+const char* gitVersion(void);
 
 #endif // PROD_TEST_HH
