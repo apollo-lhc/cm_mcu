236619b2b062d693eff5a7b8e093c83553a928e1
Peter Wittich <wittich@cornell.edu>
2026-06-02 15:03:12 -0400

    formatting

--- projects/cm_mcu/InitTask.c
+++ projects/cm_mcu/InitTask.c
@@ -48,9 +48,9 @@ void InitTask(void *parameters)
   while (getPowerControlState() != POWER_ON) {
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
-#endif                               // REV1
-  int status = init_registers_ff();  // initialize I/O expander for fireflies -- with FF monitoring via I2C in other threads, it grabs semaphore inside
-  log_info(LOG_SERVICE, "Firefly I/O expander init %s\r\n", status==0?"OK":"Fail");
+#endif                              // REV1
+  int status = init_registers_ff(); // initialize I/O expander for fireflies -- with FF monitoring via I2C in other threads, it grabs semaphore inside
+  log_info(LOG_SERVICE, "Firefly I/O expander init %s\r\n", status == 0 ? "OK" : "Fail");
   status = init_registers_clk(); // initialize I/O expander for clocks
   if (status == 0)
     log_info(LOG_SERVICE, "Clock I/O expander initialized\r\n");
