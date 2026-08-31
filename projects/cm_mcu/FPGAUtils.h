8b7fc32c11c0036b9b7d7c7ff42d64ec1690540d
Peter Wittich <wittich@cornell.edu>
2026-08-31 12:49:50 -0400

    add comments

--- projects/cm_mcu/FPGAUtils.h
+++ projects/cm_mcu/FPGAUtils.h
@@ -1,11 +1,13 @@
 /*
  * FPGAUtils.h
  *
- * Generic register accessors for the FPGA's I2C diagnostic-register block.
+ * Generic register accessors for the FPGA's I2C generic-register block.
  */
 #ifndef PROJECTS_CM_MCU_FPGAUTILS_H_
 #define PROJECTS_CM_MCU_FPGAUTILS_H_
 #include <stdint.h>
+
+#if defined(REV2) || defined(REV3)
 
 // Read/write a register in the FPGA's generic I2C diagnostic-register block
 // (behind the TCA9548 mux at 0x70 on I2C bus 5, device 0x2b).
@@ -14,5 +16,7 @@
 // (see SMBUS_get_error() in common/smbus_helper.h).
 int fpga_i2c_reg_r(int fpga, uint16_t reg, uint8_t nbytes, uint32_t *data);
 int fpga_i2c_reg_w(int fpga, uint16_t reg, uint8_t nbytes, uint32_t data);
+
+#endif // defined(REV2) || defined(REV3)
 
 #endif /* PROJECTS_CM_MCU_FPGAUTILS_H_ */
